fa1b02c2e6c43e03c694297f99ef1e3f697d47f0
VirginiaNV <natali9407@gmail.com>
2017-07-22 17:21:46 -0400

    Update d.js

--- d.js
+++ d.js
@@ -1,1 +1,16 @@
-fgrgfr
+practica 1
+(function() {
+    'use strict'
+
+var Nombre = "Natalia Almonte "; 
+var Edad ="23"; 
+var Profesion = "Est. Informatica"; 
+var Nivelprog ="2"; 
+var ExpectativasAngularJS="convertirme en desarrollador web";
+
+console.log("Nombre", nombre),console.log("Edad", edad),console.log("Profesion", profesion ),console.log("Nivelprog", nivelprog),
+console.log("ExpectativasAgularJS", expectativasangularjs);
+
+
+
+})();
